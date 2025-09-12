5d2276555851c9e96e5b19d53f905af0ed886b7c
Théo Cartegnie <theo.cartegnie@le-campus-numerique.fr>
2025-09-12 16:35:26 +0200

    Commit new test
    - Test de la qualité d'un produit selon une date donné
    - Test de la qualité d'un produit quand il est encore dans ca date limite
    - Test de la qualité d'un produit quand il arrive a sont dernier jours de péremption

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -1,5 +1,6 @@
 package com.magasin;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +8,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
 
     @Test
+    @Disabled
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         Magasin app = new Magasin(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
+
+    @Test
+    void TestDateVIP() {
+        Item[] items = new Item [] {new Item( "Pass VIP Concert",5,10)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(13, app.items[0].quality);
+        /*
+        *  Actuellement la valeur Quality ne s'incrémente pas en meme temps que les joursb n
+        * */
+
+    }
+    @Test
+    void TestItemQuality()
+    {
+        Item[] items = new Item [] {new Item( "Test",1,30)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(29, app.items[0].quality);
+    }
+
+    @Test
+    void TestDateQuality()
+    {
+        Item[] items = new Item [] {new Item( "Test",0,30)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(28, app.items[0].quality);
+    }
+    
 
 }
