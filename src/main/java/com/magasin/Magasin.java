86220ca3b75bb6eec8e9dcb7006001d6f40d2804
Théo Cartegnie <theo.cartegnie@le-campus-numerique.fr>
2025-09-12 15:22:47 +0200

    Comment "Update quality" function

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -8,15 +8,23 @@ class Magasin {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Comté")
-                    && !items[i].name.equals("Pass VIP Concert")) {
+        for (int i = 0; i < items.length; i++)
+
+        {
+    //////////////////////////////////////////STEP 1///////////////////////////////////////////////////////////////
+            if (!items[i].name.equals("Comté") && !items[i].name.equals("Pass VIP Concert"))
+            {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Kryptonite")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
-            } else {
+
+            }
+
+            else
+
+            {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
@@ -35,25 +43,26 @@ class Magasin {
                     }
                 }
             }
-
-            if (!items[i].name.equals("Kryptonite")) {
-                items[i].sellIn = items[i].sellIn - 1;
+/////////////////////////////////////////STEP 2 /////////////////////////////////////////////////
+            if (!items[i].name.equals("Kryptonite"))/*Si CE N'EST PAS de la Kryptonite*/ {
+                items[i].sellIn = items[i].sellIn - 1;//L'item perds un jour de durée de vie.
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Comté")) {
-                    if (!items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Kryptonite")) {
-                                items[i].quality = items[i].quality - 1;
+            if (items[i].sellIn < 0)/*Si on arrive a la date de peremption*/ {
+                if (!items[i].name.equals("Comté"))/*Et que l'objet n'est pas du fromage*/ {
+                    if (!items[i].name.equals("Pass VIP Concert"))/*Ni un Pass VIP de concert*/ {
+                        if (items[i].quality > 0)/*Si il reste des objets*/ {
+                            if (!items[i].name.equals("Kryptonite"))/*Et que ce n'est pas de la kryptonite*/ {
+                                items[i].quality = items[i].quality - 1;/*L'objet perds en qualité*/
                             }
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    else /*Si l'objet n'est pas une place de concert*/{
+                        items[i].quality = items[i].quality - items[i].quality /*La qualité de l'objet passe a zero (normalement)*/;
+                    }
+                } else /*Si l'objet n'est PAS du compté*/{
+                    if (items[i].quality < 50)/*Que ca quantité est en dessous de 50*/ {
+                        items[i].quality = items[i].quality + 1/*Ca qualité augement*/;
                     }
                 }
             }
